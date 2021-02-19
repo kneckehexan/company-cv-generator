b1c57193338158c9075b53e1233b04ac32cd76cc
Philip Tunbjer <philip.tunbjer@gmail.com>
2021-02-19 09:47:06 +0100

    Adding education textarea works.

--- app/www/scripts/form.js
+++ app/www/scripts/form.js
@@ -1,13 +1,13 @@
 $(document).ready(function() {
   console.log('Document ready');
   var educount = 0;
-  $('#addeducation').on('click', insertTextarea('edu',educount));
+  $('#addedu').on('click',{par1:'edu', par2:educount}, insertTextarea);
 });
 
-function insertTextarea(area, counter) {
-  $('#' + area).append('<label for="' + area + counter + '">Utbildning ' + counter + ': </label>');
-  $('#' + area).append('<textarea id="' + area + counter + '" name="'+ area + counter + '" placeholer="Namn på "' + area + '>');
-  counter++;
-  console.log([area, counter]);
+function insertTextarea(e) {
+  $('#' + e.data.par1).append('<label for="' + e.data.par1 + e.data.par2 + '">Utbildning ' + e.data.par2 + ': </label>');
+  $('#' + e.data.par1).append('<textarea id="' + e.data.par1 + e.data.par2 + '" name="'+ e.data.par1 + e.data.par2 + '" rows="2" cols="20">');
+  e.data.par2++;
+  console.log([e.data.par1, e.data.par2]);
 }
 
