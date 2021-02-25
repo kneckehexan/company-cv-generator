85b0afc27916695dcfe86a389873f0ebe4c2ec8f
Philip Tunbjer <philip.tunbjer@gmail.com>
2021-02-25 02:21:04 +0100

    Cleaned a bit, but messed up code aswell

--- app/www/app.py
+++ app/www/app.py
@@ -1,44 +1,79 @@
 #!/usr/bin/env python3
 
-from flask import Flask, flash, jsonify, redirect, render_template, request, session, g, Markup, url_for
-from flaskext.mysql import MySQL
-from flask_session import Session
 import jinja2
 import os
 import sys
 import tempfile
+import shutil
+import re
+from flask import Flask, flash, jsonify, redirect, render_template, request, session, g, Markup, url_for
+from flaskext.mysql import MySQL
+from flask_session import Session
 from werkzeug.exceptions import default_exceptions, HTTPException, InternalServerError
 from werkzeug.security import check_password_hash, generate_password_hash
 from werkzeug.utils import secure_filename
-
 from subprocess import Popen
-import shutil
 
-# Configure application
+
 app = Flask(__name__, template_folder="../www/templates/")
 
-# Added to work in local environment [CHANGE]
-app.config["SECRET_KEY"] = "helpfulhat@4dollarCREAM"
-# Ensure templates are auto-reloaded
-app.config["TEMPLATES_AUTO_RELOAD"] = True
-# For File uploads
-app.config['MAX_CONTENT_LENGTH'] = 1024 * 1024
-UPLOAD_FOLDER = 'appDock/www/tex/img/'
-ALLOWED_EXTENSIONS = {'png'}
+
+""" Constants """
+UPLOAD_FOLDER = 'appDock/www/tex/img/' # Where images are uploaded
+ALLOWED_EXTENSIONS = {'png'} # Add image extensions
+# Escaping tex syntax
+LATEX_SUBS = (
+    (re.compile(r"\\"), r"\\textbackslash"),
+    (re.compile(r"([{}_#%&$])"), r"\\\1"),
+    (re.compile(r"~"), r"\~{}"),
+    (re.compile(r"\^"), r"\^{}"),
+    (re.compile(r'"'), r"''"),
+    (re.compile(r"\.\.\.+"), r"\\ldots"),
+)
+# Set jinja2 environment in latex syntax so that it doesn't conflict with .tex
+PATH = os.path.join(os.path.dirname(__file__),'./tex')
+TEMPLATELOADER = jinja2.FileSystemLoader(searchpath=PATH)
+LATEX_JINJA_ENV = jinja2.Environment(
+    block_start_string = '((*',
+    block_end_string = '*))',
+    variable_start_string = '(((',
+    variable_end_string = ')))',
+    comment_start_string = '((=',
+    comment_end_string = '=))',
+    loader = TEMPLATELOADER,
+    autoescape = True
+)
+LATEX_JINJA_ENV.filters['escape_tex'] = escape_tex
+TEXTEMPLATE = LATEX_JINJA_ENV.get_template('template.tex')
+
+
+""" Configure application """
+app.config["SECRET_KEY"] = "helpfulhat@4dollarCREAM" # ONLY FOR LOCAL ENV
+app.config["TEMPLATES_AUTO_RELOAD"] = True # Autoreload templates
+app.config['MAX_CONTENT_LENGTH'] = 1024 * 1024 # Limit file size
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
-def allowed_file(filename):
-    return '.' in filename and \
-            filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
-
-#set up mysql config
-app.config['MYSQL_HOST'] = 'localhost'
-app.config['MYSQL_USER'] = 'root'
-app.config['MYSQL_PASSWORD'] = 'aA868097'
-app.config['MYSQL_DB'] = 'cv'
+app.config["SESSION_FILE_DIR"] = tempfile.mkdtemp() # Session to use filesystem
+app.config["SESSION_PERMANENT"] = False
+app.config["SESSION_TYPE"] = "filesystem"
+#app.config['MYSQL_HOST'] = 'localhost' # Database
+#app.config['MYSQL_USER'] = 'root'
+#app.config['MYSQL_PASSWORD'] = ''
+#app.config['MYSQL_DB'] = 'cv'
 
 
+""" MySQL """
 mysql = MySQL()
 mysql.init_app(app)
+
+
+""" Session """
+Session(app)
+
+
+def allowed_file(filename):
+    """ Make sure uploaded files have the correct extension """
+    return '.' in filename and \
+            filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
 
 #@app.teardown_appcontext
@@ -48,78 +83,42 @@ mysql.init_app(app)
 #        mysql.close()
 
 
-def queryDB(query, args=(), one=False):
-    """ Make queries to the DB within the app_context()
-    https://flask.palletsprojects.com/en/1.1.x/appcontext/
-    https://sentry.io/answers/working-outside-of-application-context/ """
-    cur = mysql.get_db().cursor()
-    cur.execute(query, args)
-    rv = cur.fetchall()
-    return (rv[0] if rv else None) if one else rv
+#def queryDB(query, args=(), one=False):
+#    """ Make queries to the DB within the app_context()
+#    https://flask.palletsprojects.com/en/1.1.x/appcontext/
+#    https://sentry.io/answers/working-outside-of-application-context/ """
+#    cur = mysql.get_db().cursor()
+#    cur.execute(query, args)
+#    rv = cur.fetchall()
+#    return (rv[0] if rv else None) if one else rv
 
 
-def updateDB(update, args=()):
-    """ insert, update or delete DB entries """
-    cur = mysql.get_db().cursor()
-    cur.execute(update, args)
-    mysql.get_db().commit()
+#def updateDB(update, args=()):
+#    """ insert, update or delete DB entries """
+#    cur = mysql.get_db().cursor()
+#    cur.execute(update, args)
+#    mysql.get_db().commit()
 
 
-# Ensure responses aren't cached
 @app.after_request
 def after_request(response):
+    """ Ensure responses aren't cached """
     response.headers["Cache-Control"] = "no-cache, no-store, must-revalidate"
     response.headers["Expires"] = 0
     response.headers["Pragma"] = "no-cache"
     return response
 
 
-# Configure session to use filesystem (instead of signed cookies)
-app.config["SESSION_FILE_DIR"] = tempfile.mkdtemp()
-app.config["SESSION_PERMANENT"] = False
-app.config["SESSION_TYPE"] = "filesystem"
-Session(app)
-
-# ref: https://flask.palletsprojects.com/en/1.1.x/patterns/sqlite3/
-#DATABASE = 'db/cv.db'
-
-
-# Define filter for jinja2
-import re
-
-LATEX_SUBS = (
-    (re.compile(r"\\"), r"\\textbackslash"),
-    (re.compile(r"([{}_#%&$])"), r"\\\1"),
-    (re.compile(r"~"), r"\~{}"),
-    (re.compile(r"\^"), r"\^{}"),
-    (re.compile(r'"'), r"''"),
-    (re.compile(r"\.\.\.+"), r"\\ldots"),
-)
-
 def escape_tex(value):
+    """ Make sure tex syntax is escaped """
     newval = value
     for pattern, replacement in LATEX_SUBS:
         newval = pattern.sub(replacement, newval)
     return newval
 
-# Set jinja2 environment in latex syntax so that it doesn't conflict with .tex
-path = os.path.join(os.path.dirname(__file__),'./tex')
-templateLoader = jinja2.FileSystemLoader(searchpath=path)
-latex_jinja_env = jinja2.Environment(
-    block_start_string = '((*',
-    block_end_string = '*))',
-    variable_start_string = '(((',
-    variable_end_string = ')))',
-    comment_start_string = '((=',
-    comment_end_string = '=))',
-    loader = templateLoader,
-    autoescape = True
-)
-
-latex_jinja_env.filters['escape_tex'] = escape_tex
-texTemplate = latex_jinja_env.get_template('template.tex')
 
 def writeTex(rendered_tex, out_pdf_path):
+    """ Render .tex and compile with latexmk """
     out_dir = 'appDock/www/tex/'
     cur_dir = os.getcwd()
     os.chdir(out_dir)
@@ -171,43 +170,11 @@ def addtoCV():
             msg['courses'] = [{'title': i, 'time': j} for i, j in zip(request.form.getlist('cou-title'), request.form.getlist('cou-time'))]
         if 'ass-title' in data:
             msg['assignments'] = [{'title': i, 'descr': j, 'time': k} for i,j,k in zip(request.form.getlist('ass-title'), request.form.getlist('ass-descr'), request.form.getlist('ass-time'))]
-        
 
-#        updateDB("""INSERT INTO 
-#                (name,
-#                description,
-#                startdate,
-#                finishdate,
-#                status,
-#                notes,
-#                client_id,
-#                company_id,
-#                consultant_id)
-#                VALUES (?,?,?,?,?,?,?,?,?)""",
-#                [
-#                    name,
-#                    description,
-#                    start,
-#                    finish,
-#                    status,
-#                    note,
-#                    clientID,
-#                    companyID,
-#                    consultantID])
-#        message = Markup("Uppdraget / projektet <b>%s</b> tillagd"
-#                % (name))
-#        flash(message, "alert-success")
-        tmp_dir = 'appDock/www/tex/img/'
-#        tmp_dir = url_for('static', filename = '/templates/tex/img/')
-        staticImg =  [os.path.join(tmp_dir, 'banner.png'),  \
-                os.path.join(tmp_dir, 'logo.png')]
-        cv = texTemplate.render(msg = msg, portrait = 'img/' + filename)
-#        print(cv, file=sys.stderr)
-#        print(cv, file=sys.stdout)
+        cv = TEXTEMPLATE.render(msg = data, portrait = 'img/' + filename)
         writeTex(cv, os.path.join('appDock/www/pdf/', 'rendered.pdf'))
         return redirect("/")
 
 
 if __name__=="__main__":
     app.run(host='0.0.0.0', debug=True)
-
